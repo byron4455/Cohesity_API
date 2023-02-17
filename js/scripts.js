2f67e1bf7641958b2abb8e099936ffe7683fea28
Byron Stephenson <byron.stephenson@coh-p2654c27xx.corp.cohesity.com>
2023-02-16 19:33:50 -0500

    tweaking api call

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,38 @@
 
-var generateBtn = document.getElementById('generateSP');
+// var generateBtn = document.getElementById('generateSP');
 
 
-userNameCred = [];
-userPassCred = [];
-userClusterCred = [];
+// userNameCred = [];
+// userPassCred = [];
+// userClusterCred = [];
 
-const submitLogin = function() {
-    userName = document.getElementById("inputUser").value;
-    userNameCred.push(userName);
-    userPass = document.getElementById("inputPassword").value;
-    userPassCred.push(userPass);
-    userCluster = document.getElementById("inputCluster").value;
-    userClusterCred.push(userCluster);
-    console.log(userNameCred);
-    console.log(userClusterCred);
-    console.log(userPassCred);
-    
-};
+// const submitLogin = function() {
+//     userName = document.getElementById("inputUser").value;
+//     userNameCred.push(userName);
+//     userPass = document.getElementById("inputPassword").value;
+//     userPassCred.push(userPass);
+//     userCluster = document.getElementById("inputCluster").value;
+//     userClusterCred.push(userCluster);
+//     console.log(userNameCred);
+//     console.log(userClusterCred);
+//     console.log(userPassCred);
+//     if (userName === '') {
+//         window.alert("Please enter valid username")
+//     } else {
+//         window.location.href = "./dashboard.html"
+//     }
+// };
 
-generateBtn.addEventListener('click', submitLogin);
+// generateBtn.addEventListener('click', submitLogin);
 
+userNameCred = localStorage.getItem('username');
+console.log('user: ' + userNameCred);
 
+userClusterCred = localStorage.getItem('cluster');
+console.log('cluster: ' + userClusterCred)
 
+userPassCred = localStorage.getItem('password');
+console.log('password: ' + userPassCred);
 const getToken = async () => {
     const response = await fetch("https://" + userClusterCred + "/irisservices/api/v1/public/accessTokens" , {
         method: 'POST',
